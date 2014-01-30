0853b7b89fc9f0c371c152c8f1dfc9882973514a
Danilo Jr. <danilobjr@gmail.com>
2014-01-30 13:07:34 -0300

    Retirada de comentários

--- app/infra/arquivo.js
+++ app/infra/arquivo.js
@@ -1,34 +1,6 @@
 var fileSystem = require("fs");
 
 exports.criar = function (path, nomeArquivo, conteudo, recriar) {
-    //fileSystem.readdir(path, function (err, files) {
-    //    if (err) {
-    //        console.log(err);
-    //    } else {
-    //        if (files) {
-    //            var arquivoJahExiste = false;
-
-    //            for (var i = 0; i < files.length; i++) {
-    //                //console.log('Encontrado: ' + files[i] + '; Procurado: ' + nomeArquivo);
-    //                if (files[i] === nomeArquivo) {
-    //                    arquivoJahExiste = true;
-    //                    //console.log('XML já existe!');
-    //                }
-    //            }
-
-    //            if (!arquivoJahExiste) {
-    //                fileSystem.writeFile(path + nomeArquivo, conteudo, function (err) {
-    //                    if (err) {
-    //                        console.log(err);
-    //                    } else {
-    //                        console.log("Arquivo criado!");
-    //                    }
-    //                });
-    //            }
-    //        }
-    //    }
-    //});
-
     var arquivos = fileSystem.readdirSync(path);
 
     if (arquivos) {
@@ -60,13 +32,3 @@ exports.obterDados = function (path, nomeArquivo) {
     var resultado = fileSystem.readFileSync(path + nomeArquivo);
     return resultado.toString('utf8');
 };
-
-//exports.obterDados = function (path, nomeArquivo, callback) {
-//    fileSystem.readFile(path + nomeArquivo, function (err, content) {
-//        if (err) {
-//            return callback(err);
-//        }
-
-//        callback(null, content);
-//    });
-//};
--- app/infra/autenticacao.js
+++ app/infra/autenticacao.js
@@ -1,25 +1,3 @@
-//var hash = require('./pass');
-
-//exports.autenticar = function (name, pass, callback) {
-//    if (!module.parent) console.log('authenticating %s:%s', name, pass);
-
-//    User.findOne({ username: name }, function (err, user) {
-//        if (user) {
-//            if (err) { 
-//                return callback(new Error('cannot find user')); 
-//            }
-
-//            hash(pass, user.salt, function (err, hash) {
-//                if (err) return callback(err);
-//                if (hash == user.hash) return callback(null, user);
-//                callback(new Error('invalid password'));
-//            });
-//        } else {
-//            return callback(new Error('cannot find user'));
-//        }
-//    });
-//};
-
 exports.autenticacaoRequerida = function (req, res, next) {
     if (req.session.user) {
         next();
@@ -29,16 +7,3 @@ exports.autenticacaoRequerida = function (req, res, next) {
         res.redirect('/login');
     }
 };
-
-//exports.usuarioExiste = function (req, res, next) {
-//    User.count({
-//        username: req.body.username
-//    }, function (err, count) {
-//        if (count === 0) {
-//            next();
-//        } else {
-//            req.session.error = "User Exist"
-//            res.redirect("/signup");
-//        }
-//    });
-//};

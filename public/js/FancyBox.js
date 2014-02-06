20197e128a15ac5a353966cf67311e6e954569b2
Danilo Jr. <danilobjr@gmail.com>
2014-02-06 14:09:34 -0300

    Home: reabilitando a aba participações em Fotos

--- public/js/FancyBox.js
+++ public/js/FancyBox.js
@@ -30,4 +30,12 @@ $(document).ready(function() {
             closeEffect: 'none',
             margin: [20, 60, 20, 60]
         });        
+
+    $('#slide_fotos_participacoes a')
+        .attr('rel', 'gallery')
+        .fancybox({
+            openEffect: 'none',
+            closeEffect: 'none',
+            margin: [20, 60, 20, 60]
+        });
 });
